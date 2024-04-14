ccc1a5fa32a120a5099e0a13673504d12bd6701b
Tuomas Korpi <tuomas.korpi@nordcloud.com>
2024-04-14 15:36:47 +0300

    CNAME to gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './components/HomePage';
+import AboutUs from './components/AboutUs';
+import ProductPage from './components/ProductPage';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about-us" component={AboutUs} />
+        <Route path="/products" component={ProductPage} />
+      </Switch>
+    </Router>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
