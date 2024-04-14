8906ceccadca12518b47a3a406cdda64d572c2c9
Tuomas Korpi <tuomas.korpi@nordcloud.com>
2024-04-14 19:22:13 +0300

    about image ok

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,11 +2,25 @@ import woodprint from '../assets/images/woodprint.png'
 import jannasanna from '../assets/images/jannasanna.png'
 
 const AboutPage = () => {
-    return ( 
-        <div className="jannasanna">
-            <img src={jannasanna} alt="Foreground" className="foreground-image" />
+    return (
+        <div className='aboutContainer'>
+            <div className="jannasanna">
+                <div className='image-container'>
+                    <img src={jannasanna} alt="Foreground" className="foreground-image" />
+                </div>
+                <div className='textArea'>
+                    <h1>
+                        Kaiken alku  <br /> ja juuri
+                    </h1>
+                    <p>
+                        Elokuussa 2020 muotoilun opintojen ensimmäisellä tunnilla satuimme istumaan vierekkäin muotoilupiirtämisen
+                        tunnille. Kaikki oli uutta, eikä tunnettu ketään. Muotoilun lehtori kysyi keitä olimme,
+                        mistä tulemme ja mihin olemme menossa. <br /><br />Meidä molempien vastauksen loppu oli sama:<br/> "Vähän tuo puu kyllä kiinnostaa..."
+                    </p>
+                </div>
+            </div>
         </div>
-      );
+    );
 }
- 
+
 export default AboutPage;
